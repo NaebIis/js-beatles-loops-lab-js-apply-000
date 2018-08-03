1aa1b5b2bd1b45192f2762bdce14fcc9e68649f7
John Grubbs <johnmlowery@gmail.com>
2018-08-03 14:07:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // add solution here
+
+function theBeatlesPlay(musicians, instruments) {
+  var x = []
+  for (let z = 0; z < 4; z++){
+  x.push(`${musicians[z]} plays ${instruments[z]}`)
+  }
+  return x
+}
+
